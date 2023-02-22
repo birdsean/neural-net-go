9b735856cfd5ac21cf1b9f55f02bbee38ac9b490
Sean Bird <bird.seanmichael@gmail.com>
2023-02-22 14:01:14 -0700

    test sigmoid prime

--- neural-net/activations_test.go
+++ neural-net/activations_test.go
@@ -1,6 +1,8 @@
 package neuralnet
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_sigmoid(t *testing.T) {
 	type args struct {
@@ -22,6 +24,31 @@ func Test_sigmoid(t *testing.T) {
 			got := sigmoid(tt.args.x)
 			if got < tt.want-0.00000000001 || got > tt.want+0.00000000001 {
 				t.Errorf("sigmoid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sigmoidPrime(t *testing.T) {
+	type args struct {
+		x float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "calculates sigmoidPrime(.88) correctly",
+			args: args{x: 0.88},
+			want: 0.20722456886192342,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := sigmoidPrime(tt.args.x)
+			if got < tt.want-0.00000000001 || got > tt.want+0.00000000001 {
+				t.Errorf("sigmoidPrime() = %v, want %v", got, tt.want)
 			}
 		})
 	}
