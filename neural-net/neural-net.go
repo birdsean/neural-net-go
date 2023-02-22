e38c49690ee6cf20b277555ef4ad7908992e5b15
Sean Bird <bird.seanmichael@gmail.com>
2023-02-22 13:45:15 -0700

    pull out helper functions to own files

--- neural-net/neural-net.go
+++ neural-net/neural-net.go
@@ -2,11 +2,7 @@ package neuralnet
 
 import (
 	"errors"
-	"math"
-	"math/rand"
-	"time"
 
-	"gonum.org/v1/gonum/floats"
 	"gonum.org/v1/gonum/mat"
 )
 
@@ -48,7 +44,7 @@ func (nn *NeuralNet) Train(inputVars, desiredOutputs *mat.Dense) error {
 	return nil
 }
 
-func (nn *NeuralNet) Predict(x *mat.Dense) (*mat.Dense, error) {
+func (nn *NeuralNet) Predict(inputVars *mat.Dense) (*mat.Dense, error) {
 	if nn.hiddenWeights == nil || nn.outputWeights == nil {
 		return nil, errors.New("the supplied weights are empty")
 	}
@@ -57,23 +53,15 @@ func (nn *NeuralNet) Predict(x *mat.Dense) (*mat.Dense, error) {
 	}
 
 	output := new(mat.Dense)
-	feedForward(x, nn.hiddenWeights, nn.hiddenBias, nn.outputWeights, nn.outputBias, output)
+	nn.feedForward(inputVars, output)
 	return output, nil
-}
-
-func sigmoid(x float64) float64 {
-	return 1.0 / (1.0 + math.Exp(-x))
-}
-
-func sigmoidPrime(x float64) float64 {
-	return sigmoid(x) * (1.0 - sigmoid(x))
 }
 
 func (nn *NeuralNet) backpropagate(inputVars, desiredOutputs, hiddenWeights, biasHidden, weightsOutput, biasOutput, output *mat.Dense) error {
 	for i := 0; i < nn.config.CountEpochs; i++ {
 
 		// feed forward
-		hiddenLayerActivations := feedForward(inputVars, hiddenWeights, biasHidden, weightsOutput, biasOutput, output)
+		hiddenLayerActivations := nn.feedForward(inputVars, output)
 
 		// backpropogate
 		networkError := new(mat.Dense)
@@ -100,32 +88,18 @@ func (nn *NeuralNet) backpropagate(inputVars, desiredOutputs, hiddenWeights, bia
 	return nil
 }
 
-func calcDerivatives(outputSignals, errorSignals *mat.Dense) *mat.Dense {
-	outputSlopes := calcTangentSlopes(outputSignals)
-	derivatives := new(mat.Dense)
-	derivatives.MulElem(errorSignals, outputSlopes)
-	return derivatives
-}
-
-func calcTangentSlopes(outputSignals *mat.Dense) *mat.Dense {
-	slopes := new(mat.Dense)
-	applySigmoidPrime := func(_, _ int, v float64) float64 { return sigmoidPrime(v) }
-	slopes.Apply(applySigmoidPrime, outputSignals)
-	return slopes
-}
-
-func feedForward(inputVars, hiddenWeights, biasHidden, weightsOutput, biasOutput, output *mat.Dense) *mat.Dense {
+func (nn *NeuralNet) feedForward(inputVars, output *mat.Dense) *mat.Dense {
 	summingJunction := new(mat.Dense)
-	summingJunction.Mul(inputVars, hiddenWeights)
-	summingJunction.Apply(func(i, j int, v float64) float64 { return v + biasHidden.At(0, j) }, summingJunction)
+	summingJunction.Mul(inputVars, nn.hiddenWeights)
+	summingJunction.Apply(func(i, j int, v float64) float64 { return v + nn.hiddenBias.At(0, j) }, summingJunction)
 
 	nonLinearActivations := new(mat.Dense)
 	applySigmoid := func(_, _ int, v float64) float64 { return sigmoid(v) }
 	nonLinearActivations.Apply(applySigmoid, summingJunction)
 
 	outputLayerInput := new(mat.Dense)
-	outputLayerInput.Mul(nonLinearActivations, weightsOutput)
-	outputLayerInput.Apply(func(i, j int, v float64) float64 { return v + biasOutput.At(0, j) }, outputLayerInput)
+	outputLayerInput.Mul(nonLinearActivations, nn.outputWeights)
+	outputLayerInput.Apply(func(i, j int, v float64) float64 { return v + nn.outputBias.At(0, j) }, outputLayerInput)
 	output.Apply(applySigmoid, outputLayerInput)
 	return nonLinearActivations
 }
@@ -138,50 +112,8 @@ func (nn *NeuralNet) adjustWeights(originalWeights, activations, derivative *mat
 }
 
 func (nn *NeuralNet) adjustBias(original, derivative *mat.Dense) error {
-	adjustedbiasOutput, err := sumAlongAxis(0, derivative)
-	if err != nil {
-		return err
-	}
+	adjustedbiasOutput := sumAlongColumn(derivative)
 	adjustedbiasOutput.Scale(nn.config.LearningRate, adjustedbiasOutput)
 	original.Add(original, adjustedbiasOutput)
 	return nil
-}
-
-func sumAlongAxis(axis int, m *mat.Dense) (*mat.Dense, error) {
-
-	numRows, numCols := m.Dims()
-
-	var output *mat.Dense
-
-	switch axis {
-	case 0:
-		data := make([]float64, numCols)
-		for i := 0; i < numCols; i++ {
-			col := mat.Col(nil, i, m)
-			data[i] = floats.Sum(col)
-		}
-		output = mat.NewDense(1, numCols, data)
-	case 1:
-		data := make([]float64, numRows)
-		for i := 0; i < numRows; i++ {
-			row := mat.Row(nil, i, m)
-			data[i] = floats.Sum(row)
-		}
-		output = mat.NewDense(numRows, 1, data)
-	default:
-		return nil, errors.New("invalid axis, must be 0 or 1")
-	}
-
-	return output, nil
-}
-
-func randPopulateMatrices(matrices ...*mat.Dense) {
-	randomSource := rand.NewSource(time.Now().UnixNano())
-	randomGenerator := rand.New(randomSource)
-	for _, matrix := range matrices {
-		rawMatrix := matrix.RawMatrix().Data
-		for i := range rawMatrix {
-			rawMatrix[i] = randomGenerator.Float64()
-		}
-	}
 }
